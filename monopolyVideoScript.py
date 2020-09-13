1cfc5019e3774b642b112a9c5b52da304f173c8b
h4nsec <66410856+h4nsec@users.noreply.github.com>
2020-09-14 08:16:37 +1000

    GPIO Video Code
    Rough, untested Python Script to play videos when GPIO's are short to ground.

---
+++ monopolyVideoScript.py
@@ -0,0 +1,99 @@
+#! usr/bin/env python3
+
+import Rpi.GPIO as GPIOimport sysimport osfrom subprocess 
+import Popen
+import os, sys
+
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Define Video Locations
+movie1 = ("/home/pi/Monopoly/movie1.mp4")
+movie2 = ("/home/pi/Monopoly/movie2.mp4")
+chance = random.choice(os.listdir("/home/pi/Monopoly/Chance"))
+chest = random.choice(os.listdir("/home/pi/Monopoly/Chest"))
+
+# Set States
+last_state1 = True
+last_state2 = True
+last_state3 = True
+last_state4 = True
+
+input_state1 = True
+input_state2 = True
+input_state3 = True
+input_state4 = True
+
+quit_video = True
+
+
+while True:
+	#Read states of inputs
+	input_state1 = GPIO.input(17)
+	input_state2 = GPIO.input(18)
+	quite_video = GPIO.input(24)
+
+	#If GPIO(17) is shorted to ground
+	if input_state1 != last_state1:
+		if (player and not input_state1):
+			os.system('killall omxplayer.bin')
+			omxc = Popen(['omxplayer', '-b', movie1])
+			player = True
+		elif not input_state1:
+			omxc = Popen(['omxplayer', '-b', movie1])
+			player = True
+	
+	#If GPIO(18) is shorted to ground
+	elif input_state2 != last_state2:
+		if (player and not input_state2):
+			os.system('killall omxplayer.bin')
+			omxc = Popen(['omxplayer', '-b', movie2])
+			player = True
+		elif not input_state2:
+			omxc = Popen(['omxplayer', '-b', movie2])
+			player = True
+			
+	#If GPIO(19) is shorted to ground
+	elif input_state3 != last_state3:
+		if (player and not input_state3):
+			os.system('killall omxplayer.bin')
+			omxc = Popen(['omxplayer', '-b', chance])
+			player = True
+		elif not input_state3:
+			omxc = Popen(['omxplayer', '-b', chance])
+			player = True
+			
+			
+	#If GPIO(20) is shorted to ground		
+	elif input_state4 != last_state4:
+		if (player and not input_state4):
+			os.system('killall omxplayer.bin')
+			omxc = Popen(['omxplayer', '-b', chest])
+			player = True
+		elif not input_state4:
+			omxc = Popen(['omxplayer', '-b', chest])
+			player = True
+
+			
+			
+
+	#If omxplayer is running and NO GPIO are shorted to ground
+	elif (player and input_state1 and input_state2):
+		os.system('killall omxplayer.bin')
+		player = False
+
+	#GPIO(24) to close omxplayer manually - used during debug
+	if quit_video == False:
+		os.system('killall omxplayer.bin')
+		player = False
+
+	#Set last_input states
+	last_state1 = input_state1
+	last_state2 = input_state2 
+	last_state3 = input_state3
+	last_state4 = input_state4
