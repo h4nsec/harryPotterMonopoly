b880527c0c23ad2d0790488a080a7ca57c8fe1fd
h4nsec <66410856+h4nsec@users.noreply.github.com>
2020-09-15 09:16:51 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,40 @@
-#!/usr/bin/env python3
+#! usr/bin/env python3
 
+import RPi.GPIO as GPIO
 import os
 import sys
+from subprocess import Popen
 import random
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 chancePath = "/home/pi/Monopoly/Chance/"
 
 def chance():
-    print(chancePath + random.choice(os.listdir("/home/pi/Monopoly/Chance/")))
-  
-chance()
+	chancePath + random.choice(os.listdir("/home/pi/Monopoly/Chance/"))
+
+last_state3 = True
+input_state3 = True
+quit_video = True
+player = False
+
+while True:
+    input_state3 = GPIO.input(27)
+    
+    #If GPIO(27) is shorted to ground
+        elif input_state3 != last_state3:
+            if (player and not input_state3):
+                os.system('killall omxplayer.bin')
+                omxc = Popen(['omxplayer', '-b', chance()])
+                player = True
+            elif not input_state3:
+                omxc = Popen(['omxplayer', '-b', chance()])
+                player = True
+
+
+last_state3 = input_state3           
+            
+           
