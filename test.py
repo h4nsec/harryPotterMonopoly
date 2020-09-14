b6dc9308918e28e62458c6c9e6bf2d332347f5f3
h4nsec <66410856+h4nsec@users.noreply.github.com>
2020-09-15 09:01:10 +1000

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,11 @@
 
 import os
 import sys
+import random
+
+chancePath = "/home/pi/Monopoly/Chance/"
 
 def chance():
-  print(random.choice(os.listdir("/home/pi/Monopoly/Chance/")))
+    print(chancePath + random.choice(os.listdir("/home/pi/Monopoly/Chance/")))
   
 chance()
