c26cc54b1f71f300f8022f829bcac1c7950e5f16
h4nsec <66410856+h4nsec@users.noreply.github.com>
2020-09-15 08:57:17 +1000

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import os
 import sys
 
 def chance():
-  random.choice(os.listdir(/home/pi/Monopoly/Chance/"))
+  print(random.choice(os.listdir(/home/pi/Monopoly/Chance/")))
   
-print(chance)
+chance()
