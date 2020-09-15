ddba892470a33d08d1bd524ca201789933c43486
h4nsec <66410856+h4nsec@users.noreply.github.com>
2020-09-15 10:34:39 +1000

    Update test.py

--- test.py
+++ test.py
@@ -26,15 +26,12 @@ while True:
     
     #If GPIO(27) is shorted to ground
     if input_state3 != last_state3:
-	if (player and not input_state3):
-		os.system('killall omxplayer.bin')
-		omxc = Popen(['omxplayer', '-b', str(chance())])
-		print(chance())
-		player = True
-	elif not input_state3:
-		omxc = Popen(['omxplayer', '-b', str(chance())])
-		player = True
+      if (player and not input_state3):
+        os.system('killall omxplayer.bin')
+        omxc = Popen(['omxplayer', '-b', str(chance())])
+        player = True
+      elif not input_state3:
+        omxc = Popen(['omxplayer', '-b', str(chance())])
+        player = True
 		
-last_state3 = input_state3           
-            
-           
+last_state3 = input_state3         
